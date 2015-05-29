e9415a36d8ccac480280b7cf2b6ab51aacda6054
SC <github.com@genkisugi.net>
2015-05-28 19:25:44 -0700

    annotation overlay onto graph objects; tests; work on #1

--- tests/noctua-basic.tests.js
+++ tests/noctua-basic.tests.js
@@ -15,4 +15,43 @@ describe('test annotation', function(){
 	assert.equal(a1.property('foo'), 'bar', 'has prop');
 	assert.equal(a1.property('bar'), null, 'does not have prop');
     });
+
+});
+
+describe('annotation bulk ops', function(){
+
+    it('graph context extant', function(){
+	var g = new model.graph();
+	assert.isFunction(g.get_annotation_by_id, 'annotation attached to');
+    });
+
+    it('works in graph context', function(){
+
+	var g = new model.graph();
+
+	var a1 = new model.annotation({'key': 'foo', 'value': 'bar'});
+	var a2 = new model.annotation({'key': 'bib', 'value': 'bab'});
+
+	var a1_id = a1.id()
+	var a2_id = a2.id()
+
+	g.add_annotation(a1);
+	g.add_annotation(a2);
+
+	assert.equal(a1_id, g.get_annotation_by_id(a1_id).id(),
+		     'get annotation by id');
+	assert.equal(g.annotations().length, 2,
+		     'two annotations in');
+
+	function filter(ann){
+	    var ret = false;
+	    if( ann.property('foo') ){
+		ret = true;
+	    }
+	    return ret;
+	}
+	assert.equal(g.get_annotations_by_filter(filter).length, 1,
+		     'one annotation by filter');
+    });
+
 });
